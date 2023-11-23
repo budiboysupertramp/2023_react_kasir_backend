c141e51a68aaa08068c4151c67f640e6cd6501b5
Setia Budi <budiboy195@gmail.com>
2023-11-23 10:45:46 +0700

    testing server

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors"); 
 
+server.use(
+  cors({
+    origin: "https://kasir-frontend.vercel.app",
+    credentials: true, // Mengizinkan pengiriman cookies
+  })
+);
+// server.use(cors());
 server.use(middlewares);
 server.use(router);
 
